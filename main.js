99aee69d88181310f076dd4741ba718492b8e4c3
Hernan Nataniel <cancinorobleshernan@gmail.com>
2023-08-09 13:13:23 -0600

    js

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('Hola mundo');
+
+var nombre="hernan c";
+var apellido="cancino";
+
+var concatenacion= nombre+" "+apellido;
+
+var dato = document.getElementById("dato");
+
+dato.innerHTML = concatenacion;
